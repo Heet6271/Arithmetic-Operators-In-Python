9f9e7057d121fd430b5efbf9f1dc971837161c24
Heet Thummar <thummarhit6271@gmail.com>
2023-07-12 10:11:20 +0530

    P5

---
+++ Prac5.py
@@ -0,0 +1,17 @@
+a=int(input('Enter value of a:'))
+b=int(input('Enter value of b:'))
+
+c= a + b
+print(c)
+d= a - b
+print(d)
+e= a * b
+print(e)
+f= a / b
+print(f)
+g= a % b
+print(g)
+h= a//b
+print(h)
+i=a ** b
+print(i)
